e2fabdabb00c965a3762c7602eb8499b51ecdf66
Antonio Saiful Islam <finallyantonio@gmail.com>
2022-05-07 00:42:45 +0700

    Add validator

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
 import { SigninDto, SignupDto } from "./dto";
 import * as argon from "argon2";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
 @Injectable({})
 
 export class AuthService {
@@ -24,26 +25,34 @@ export class AuthService {
         // generate the hash
         const hash = await argon.hash(dto.password);
 
-        console.log(hash);
+        try {
+            // save the new user in the db
+            const user = await this.prisma.user.create({
+                data: {
+                    email: dto.email,
+                    firstName: dto.firstName,
+                    lastName: dto.lastName,
+                    hash
+                }
+            });
 
-        // save the new user in the db
-        const user = await this.prisma.user.create({
-            data: {
-                email: dto.email,
-                firstName: dto.firstName,
-                lastName: dto.lastName,
-                hash
+            delete user.hash;
+
+            // return th esaved user
+
+            return {
+                statusCode: 200,
+                message: 'SignUp Successfully',
+                data: user,
+            };
+        } catch (error) {
+            if(error instanceof PrismaClientKnownRequestError) {
+                if (error.code === 'P2002') {
+                    throw new ForbiddenException('Credentials invalid');
+                }
             }
-        });
+            throw error;
+        }
 
-        delete user.hash;
-
-        // return th esaved user
-
-        return {
-            statusCode: 200,
-            message: 'SignUp Successfully',
-            data: user,
-        };
     }
 }
