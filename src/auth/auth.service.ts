e470e8b795e08fa37fdf0099815b24c856830dbe
Antonio Saiful Islam <finallyantonio@gmail.com>
2022-05-06 11:20:54 +0700

    Enhance Prisma Module & Integrate with Auth Module

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
 
 @Injectable({})
 
 export class AuthService {
+
+    constructor(private prisma: PrismaService) {
+
+    }
+
     signin() {
         return {
             statusCode: 200,
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+import { env } from 'process';
 
 @Injectable()
-export class PrismaService {}
+export class PrismaService extends PrismaClient {
+    constructor() {
+        super({
+            datasources: {
+                db: {
+                    url: "postgresql://postgres:mysecretpassword@localhost:5432/mcrae?schema=public",
+                }
+            }
+        })
+    }
+}
