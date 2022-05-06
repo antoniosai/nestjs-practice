6b069e980d90c75becdb5ae30eec93e85a7f3d16
Antonio Saiful Islam <finallyantonio@gmail.com>
2022-05-07 00:31:07 +0700

    Add prefix to url mdules

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,7 @@
 import { Body, Controller, ParseIntPipe, Post } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { SigninDto, SignupDto } from "./dto";
-@Controller()
+@Controller('/auth')
 export class AuthController {
     constructor(private authService: AuthService) {}
 
