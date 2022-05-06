9b66c975070fb957ab0c9231f653904ea01f0afe
Antonio Saiful Islam <finallyantonio@gmail.com>
2022-05-06 10:49:16 +0700

    Enhance service

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,17 @@
-import { Controller, Module } from "@nestjs/common";
-
-
-@Controller({})
+import { Controller, Module, Post } from "@nestjs/common";
+import { AuthService } from "./auth.service";
+@Controller()
 export class AuthController {
+    constructor(private authService: AuthService) {
+    }
 
+    @Post('signin')
+    signin() {
+        return this.authService.signin();
+    }
+
+    @Post('signup')
+    signup() {
+        return this.authService.signin();
+    }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,5 +3,19 @@ import { Injectable } from "@nestjs/common";
 @Injectable({})
 
 export class AuthService {
+    signin() {
+        return {
+            statusCode: 200,
+            message: 'SignIn Successfully',
+            data: null,
+        };
+    }
 
+    signup() {
+        return {
+            statusCode: 200,
+            message: 'SignUp Successfully',
+            data: null,
+        };
+    }
 }
