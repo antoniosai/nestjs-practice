9c040bed1e92d8aa3405a90319a77b9b9b56998d
Antonio Saiful Islam <finallyantonio@gmail.com>
2022-06-06 02:30:30 +0700

    Fix migration

--- prisma/migrations/20220605192848_/migration.sql
+++ prisma/migrations/20220605192848_/migration.sql
@@ -44,6 +44,7 @@ CREATE TABLE "satuan" (
 CREATE TABLE "produk" (
     "id" SERIAL NOT NULL,
     "stok" INTEGER NOT NULL DEFAULT 0,
+    "slug" TEXT NOT NULL,
     "nama" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
@@ -51,6 +52,28 @@ CREATE TABLE "produk" (
     "satuanId" INTEGER NOT NULL,
 
     CONSTRAINT "produk_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "bahan" (
+    "id" SERIAL NOT NULL,
+    "stok" INTEGER NOT NULL DEFAULT 0,
+    "slug" TEXT NOT NULL,
+    "nama" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "kategoriId" INTEGER NOT NULL,
+    "satuanId" INTEGER NOT NULL,
+
+    CONSTRAINT "bahan_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "bahan_on_produk" (
+    "bahanId" INTEGER NOT NULL,
+    "produkId" INTEGER NOT NULL,
+
+    CONSTRAINT "bahan_on_produk_pkey" PRIMARY KEY ("bahanId","produkId")
 );
 
 -- CreateIndex
@@ -65,6 +88,12 @@ CREATE UNIQUE INDEX "kategori_slug_key" ON "kategori"("slug");
 -- CreateIndex
 CREATE UNIQUE INDEX "satuan_slug_key" ON "satuan"("slug");
 
+-- CreateIndex
+CREATE UNIQUE INDEX "produk_slug_key" ON "produk"("slug");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "bahan_slug_key" ON "bahan"("slug");
+
 -- AddForeignKey
 ALTER TABLE "users" ADD CONSTRAINT "users_roleId_fkey" FOREIGN KEY ("roleId") REFERENCES "roles"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
@@ -73,3 +102,15 @@ ALTER TABLE "produk" ADD CONSTRAINT "produk_kategoriId_fkey" FOREIGN KEY ("kateg
 
 -- AddForeignKey
 ALTER TABLE "produk" ADD CONSTRAINT "produk_satuanId_fkey" FOREIGN KEY ("satuanId") REFERENCES "satuan"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "bahan" ADD CONSTRAINT "bahan_kategoriId_fkey" FOREIGN KEY ("kategoriId") REFERENCES "kategori"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "bahan" ADD CONSTRAINT "bahan_satuanId_fkey" FOREIGN KEY ("satuanId") REFERENCES "satuan"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "bahan_on_produk" ADD CONSTRAINT "bahan_on_produk_produkId_fkey" FOREIGN KEY ("produkId") REFERENCES "produk"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "bahan_on_produk" ADD CONSTRAINT "bahan_on_produk_bahanId_fkey" FOREIGN KEY ("bahanId") REFERENCES "bahan"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
